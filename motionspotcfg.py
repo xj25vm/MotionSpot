28cd6b705ab2ee2409ac548db384083ada21d0c2
Sebastian Arcus <github@open-t.co.uk>
2025-02-03 01:17:03 +0000

    Update motionspotcfg.py

--- motionspotcfg.py
+++ motionspotcfg.py
@@ -1,7 +1,8 @@
 # list ips or hostnames of cameras if controlling the led or settings on them
 cameras = {
-    'front_door' : {
-        'host' : 'first_camera_host_name_or_ip',
+    'first_camera_name' : {
+        # hostname or ip address of camera
+        'host' : '192.168.1.58',
         # site information to be attached to the alerts
         'camera_site_name' : 'At Home',
         # classes of objects to be detected for presence
@@ -10,8 +11,9 @@ cameras = {
         'detect_count_classes' : ['bicycle'],
         # classes of objects to be detected for motion
         'detect_motion_classes' : ['car', 'bus'],
-        # per camera settings
+        # turn led spotlight on for this camera when detecting an object
         'control_led' : True,
+        # destinations to use when sending detection alerts for this camera
         'active_alerts' : ['user1_telegram'],
     },
 }
@@ -20,12 +22,11 @@ alerts = {
     'user1_telegram' : {
         # alert type: email, telegream, ntfy
         'alert_type' : 'telegram',
-
         # resize images before sending - optional
         'alert_image_resize' : 0.2,
-        # details for sending telegram alerts
-        'alert_telegram_bot_token' : 'long_telegram_bot_token',
-        # get telegram user_id by starting by searching and starting a conversation with the @userinfobot bot
+        # telegram bot token - have to create this first using BotFather if it doesn't exist
+        'alert_telegram_bot_token' : 'telegram_bot_token',
+        # get telegram user_id by searching and starting a conversation with the @userinfobot bot
         'alert_telegram_user_id' : 'telegram_id_of_recipient',
     },
 
@@ -46,12 +47,13 @@ alerts = {
 camera_username = 'admin'
 camera_password = 'very_secure_password'
 
-# path where Motioneye saves snapshots - for sending to object detector
-img_path = '/var/lib/motioneye/cctv/images'
+# path where Motion saves snapshots - for sending to object detector
+img_path = '/var/lib/motion/cctv/images'
 # log path - required - path to file or 'stdout'
-log_path = '/var/log/motioneye/motionspot.log'
+log_path = '/var/log/motion/motionspot.log'
 # log level: e, w, i, d (error, warning, info, debug) - defaults to 'e'
 log_level = 'd'
 # store pid of detection loop process here
-lock_path = '/var/lib/motioneye/locks'
-yolo_model = '/etc/motioneye/yolov8s.pt' 
+lock_path = '/var/lib/motion/locks'
+# load yolo model from here - best to download it and save it before running the script
+yolo_model = '/etc/motion/yolov8s.pt' 
